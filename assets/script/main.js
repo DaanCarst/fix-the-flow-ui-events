9f9f2e8248d0f559f2183eb1d83a7e54ff71c842
Daan Carstens <daanbrother@icloud.com>
2023-01-17 11:39:44 +0100

    handler fixes

--- assets/script/main.js
+++ assets/script/main.js
@@ -64,24 +64,24 @@ document.addEventListener('long-press', function(e) {
 
 !function(t,e){"use strict";function n(){this.dispatchEvent(new CustomEvent("long-press",{bubbles:!0,cancelable:!0})),clearTimeout(o),console&&console.log&&console.log("long-press fired on "+this.outerHTML)}var o=null,u="ontouchstart"in t||navigator.MaxTouchPoints>0||navigator.msMaxTouchPoints>0,s=u?"touchstart":"mousedown",i=u?"touchcancel":"mouseout",a=u?"touchend":"mouseup",c=u?"touchmove":"mousemove";"initCustomEvent"in e.createEvent("CustomEvent")&&(t.CustomEvent=function(t,n){n=n||{bubbles:!1,cancelable:!1,detail:void 0};var o=e.createEvent("CustomEvent");return o.initCustomEvent(t,n.bubbles,n.cancelable,n.detail),o},t.CustomEvent.prototype=t.Event.prototype),e.addEventListener(s,function(t){var e=t.target,u=parseInt(e.getAttribute("data-long-press-delay")||"1500",10);o=setTimeout(n.bind(e),u)}),e.addEventListener(a,function(t){clearTimeout(o)}),e.addEventListener(i,function(t){clearTimeout(o)}),e.addEventListener(c,function(t){clearTimeout(o)})}(this,document);
 
-flow.addEventListener('mouseup', flowHandler)
+flow.addEventListener('mouseenter', flowHandler)
 flow.addEventListener('animationend', flowHandler)
 
 function flowHandler() {
   flow.classList.toggle('flow')
 }
 
-user.addEventListener('touchend', flowHandler)
-user.addEventListener('animationend', flowHandler)
+user.addEventListener('touchend', userHandler)
+user.addEventListener('animationend', userHandler)
 
-function flowHandler() {
+function userHandler() {
   user.classList.toggle('touchend')
 }
 
-interface.addEventListener('touchstart', flowHandler)
-interface.addEventListener('animationend', flowHandler)
+interface.addEventListener('touchstart', interfaceHandler)
+interface.addEventListener('animationend', interfaceHandler)
 
-function flowHandler() {
+function interfaceHandler() {
   interface.classList.toggle('touchstart')
 }
 
